4d5de0b9f0b7cb48d8744bba905b8aa781678ee7
mejdi14 <mejdiclubiste@gmail.com>
2023-05-30 00:09:04 +0200

    two stacked cards and one is a bit off

--- app/src/main/java/com/example/lean_overlay/MainActivity.kt
+++ app/src/main/java/com/example/lean_overlay/MainActivity.kt
@@ -3,13 +3,19 @@ package com.example.lean_overlay
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.lean_overlay.ui.theme.LeanOverlayTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +28,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    StackedCards()
                 }
             }
         }
@@ -30,14 +36,28 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun StackedCards() {
+    Box(contentAlignment = Alignment.Center) {
+        Card(
+            modifier = Modifier.offset(y = 10.dp),
+            backgroundColor = Color.Red,
+        ) {
+            // Content of the card
+        }
+
+        Card(
+            modifier = Modifier.offset(y = -10.dp),
+            backgroundColor = Color.Blue,
+        ) {
+            // Content of the card
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     LeanOverlayTheme {
-        Greeting("Android")
+        StackedCards()
     }
 }
